6987fd65b0a144aca4118acbedaf82886bc17e08
Brook Heisler <redattack34@gmail.com>
2013-11-06 18:17:50 -0600

    Fixed bug where player couldn't be harmed by own explosive shots, and death messages from explosive shots would not display the shooter in the death message.

--- src/com/castlebravostudios/rayguns/entities/effects/ExplosiveEffect.scala
+++ src/com/castlebravostudios/rayguns/entities/effects/ExplosiveEffect.scala
@@ -8,9 +8,22 @@ import net.minecraft.world.World
 import com.castlebravostudios.rayguns.entities.BaseBeamEntity
 import com.castlebravostudios.rayguns.entities.TriggerOnDeath
 import com.castlebravostudios.rayguns.utils.Extensions.WorldExtension
+import net.minecraft.entity.item.EntityTNTPrimed
+import net.minecraft.entity.EntityLivingBase
 
 trait ExplosiveEffect extends Entity with BaseEffect with TriggerOnDeath {
   self : Shootable =>
+
+  /**
+   * Fake 'EntityTNTPrimed' which is used to give the correct owner when causing
+   * the explosion.
+   */
+  private class FakeTNTPrimed( world : World ) extends EntityTNTPrimed( world ) {
+    override def getTntPlacedBy = shooter match {
+      case living : EntityLivingBase => living
+      case _ => null //Should never happen
+    }
+  }
 
   def colourRed : Float = 0.5f
   def colourBlue : Float = 0.5f
@@ -21,7 +34,7 @@ trait ExplosiveEffect extends Entity with BaseEffect with TriggerOnDeath {
   def createImpactParticles(hitX : Double, hitY : Double, hitZ: Double) = ()
 
   def triggerAt( x : Double, y : Double, z : Double ) = if ( worldObj.isOnServer ) {
-    worldObj.newExplosion(shooter, x, y, z, 4, false, true)
+    worldObj.newExplosion(new FakeTNTPrimed(this.worldObj), x, y, z, 3.0f, false, true)
   }
 }
 
