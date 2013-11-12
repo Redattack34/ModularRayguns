5ff2a96080b90f1c45a1ba6e2882494dc214165f
Brook Heisler <redattack34@gmail.com>
2013-11-11 19:19:39 -0600

    Added death ray effect.

--- src/com/castlebravostudios/rayguns/items/chambers/DeathRayChamber.scala
+++ src/com/castlebravostudios/rayguns/items/chambers/DeathRayChamber.scala
@@ -2,21 +2,19 @@ package com.castlebravostudios.rayguns.items.chambers
 
 import com.castlebravostudios.rayguns.api.BeamRegistry
 import com.castlebravostudios.rayguns.api.items.ItemChamber
-import com.castlebravostudios.rayguns.items.emitters.LaserEmitter
+import com.castlebravostudios.rayguns.entities.effects.DeathRayBeamEntity
+import com.castlebravostudios.rayguns.entities.effects.DeathRayBoltEntity
+import com.castlebravostudios.rayguns.items.emitters.DeathRayEmitter
+import com.castlebravostudios.rayguns.items.lenses.PreciseBeamLens
 import com.castlebravostudios.rayguns.items.lenses.PreciseLens
 import com.castlebravostudios.rayguns.items.lenses.WideLens
 import com.castlebravostudios.rayguns.mod.Config
+import com.castlebravostudios.rayguns.utils.BeamUtils
 import com.castlebravostudios.rayguns.utils.BoltUtils
 import com.castlebravostudios.rayguns.utils.GunComponents
 import com.castlebravostudios.rayguns.utils.RecipeRegisterer
+
 import net.minecraft.item.Item
-import com.castlebravostudios.rayguns.items.lenses.PreciseBeamLens
-import com.castlebravostudios.rayguns.utils.BeamUtils
-import net.minecraft.client.particle.EntityFX
-import com.castlebravostudios.rayguns.entities.BaseBeamEntity
-import com.castlebravostudios.rayguns.entities.effects.LaserBoltEntity
-import com.castlebravostudios.rayguns.entities.effects.LaserBeamEntity
-import com.castlebravostudios.rayguns.items.emitters.DeathRayEmitter
 
 object DeathRayChamber extends Item( Config.chamberDeathRay ) with ItemChamber {
 
@@ -30,18 +28,18 @@ object DeathRayChamber extends Item( Config.chamberDeathRay ) with ItemChamber {
 
   BeamRegistry.register({
     case GunComponents(_, DeathRayChamber, _, None, _) => { (world, player) =>
-      BoltUtils.spawnNormal( world, new LaserBoltEntity(world), player )
+      BoltUtils.spawnNormal( world, new DeathRayBoltEntity(world), player )
     }
     case GunComponents(_, DeathRayChamber, _, Some(PreciseLens), _ ) => { (world, player) =>
-      BoltUtils.spawnPrecise( world, new LaserBoltEntity( world ), player )
+      BoltUtils.spawnPrecise( world, new DeathRayBoltEntity( world ), player )
     }
     case GunComponents(_, DeathRayChamber, _, Some(WideLens), _ ) => { (world, player) =>
       BoltUtils.spawnScatter(world, player, 9, 5 ){ () =>
-        new LaserBoltEntity(world)
+        new DeathRayBoltEntity(world)
       }
     }
     case GunComponents(_, DeathRayChamber, _, Some(PreciseBeamLens), _ ) => { (world, player) =>
-      BeamUtils.spawnSingleShot( new LaserBeamEntity(world), world, player )
+      BeamUtils.spawnSingleShot( new DeathRayBeamEntity(world), world, player )
     }
   })
 }
