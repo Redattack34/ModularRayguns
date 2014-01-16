46edb8dc308f98525932b02481ca920717129e10
Brook Heisler <redattack34@gmail.com>
2014-01-15 20:36:40 -0600

    Extracted Thermal Expansion power support into a trait.

--- src/main/scala/com/castlebravostudios/rayguns/items/misc/RayGun.scala
+++ src/main/scala/com/castlebravostudios/rayguns/items/misc/RayGun.scala
@@ -8,6 +8,7 @@ import com.castlebravostudios.rayguns.items.lenses.ChargeBeamLens
 import com.castlebravostudios.rayguns.items.lenses.ChargeLens
 import com.castlebravostudios.rayguns.mod.Config
 import com.castlebravostudios.rayguns.mod.ModularRayguns
+import com.castlebravostudios.rayguns.plugins.te.RFPowerConnector
 import com.castlebravostudios.rayguns.utils.Extensions.WorldExtension
 import com.castlebravostudios.rayguns.utils.FireEvent
 import com.castlebravostudios.rayguns.utils.GunComponents
@@ -19,19 +20,11 @@ import net.minecraft.item.ItemStack
 import net.minecraft.util.Icon
 import net.minecraft.world.World
 
-import cofh.api.energy.IEnergyContainerItem
-import cpw.mods.fml.common.Optional
-
-@Optional.Interface(iface="cofh.api.energy.IEnergyContainerItem", modid="Mekanism", striprefs=true)
-object RayGun extends ScalaItem( Config.rayGun ) with MoreInformation with IEnergyContainerItem {
+object RayGun extends ScalaItem( Config.rayGun ) with MoreInformation with RFPowerConnector {
 
   private val maxChargeTime : Double = 3.0d
   private val ticksPerSecond : Int = 20
   private val maxChargeTicks : Int = ( maxChargeTime * ticksPerSecond ).toInt
-
-  private val maxPowerTransferPerTick = 2
-
-  private val rfPowerMultiplier : Double = Config.rfPowerMultiplier
 
   import RaygunNbtUtils._
 
@@ -158,41 +151,4 @@ object RayGun extends ScalaItem( Config.rayGun ) with MoreInformation with IEner
       case _ => 0
     }
   }
-
-  @Optional.Method( modid = "Mekanism" )
-  def receiveEnergy(container : ItemStack, maxReceive : Int, simulate : Boolean ) : Int = {
-
-    val maxReceivable = (maxReceive / rfPowerMultiplier).toInt
-    val capacity = getChargeDepleted(container)
-    val energyExtracted = Math.min( capacity, Math.min( maxReceivable, maxPowerTransferPerTick ))
-
-    if ( !simulate ) {
-      addCharge(energyExtracted, container)
-    }
-
-    if ( maxReceive == 1 && simulate ) 1 else (energyExtracted * rfPowerMultiplier).toInt
-  }
-
-  @Optional.Method( modid = "Mekanism" )
-  def extractEnergy(container : ItemStack, maxExtract : Int, simulate : Boolean ) : Int = {
-    val maxExtractable = (maxExtract / rfPowerMultiplier).toInt
-    val stored = getMaxCharge(container) - getChargeDepleted(container)
-    val energyExtracted = Math.min( stored, Math.min( maxExtract, maxPowerTransferPerTick ))
-
-    if ( !simulate ) {
-      addCharge(-energyExtracted, container)
-    }
-
-    if ( maxExtract == 1 && simulate ) 1 else (energyExtracted * rfPowerMultiplier).toInt
-  }
-
-  @Optional.Method( modid = "Mekanism" )
-  def getEnergyStored(container : ItemStack) : Int = {
-    val charge = getMaxCharge(container) - getChargeDepleted(container)
-    (charge * rfPowerMultiplier).toInt
-  }
-
-  @Optional.Method( modid = "Mekanism" )
-  def getMaxEnergyStored(container : ItemStack) : Int =
-    (RaygunNbtUtils.getMaxCharge(container) * rfPowerMultiplier).toInt
 }
---
+++ src/main/scala/com/castlebravostudios/rayguns/plugins/te/RFPowerConnector.scala
@@ -0,0 +1,53 @@
+package com.castlebravostudios.rayguns.plugins.te
+
+import cofh.api.energy.IEnergyContainerItem
+import com.castlebravostudios.rayguns.utils.RaygunNbtUtils
+import net.minecraft.item.ItemStack
+import cpw.mods.fml.common.Optional
+import com.castlebravostudios.rayguns.mod.Config
+
+@Optional.Interface(iface="cofh.api.energy.IEnergyContainerItem", modid="Mekanism", striprefs=true)
+trait RFPowerConnector extends IEnergyContainerItem {
+  import RaygunNbtUtils._
+
+  private val maxPowerTransferPerTick = 2
+
+  private val rfPowerMultiplier : Double = Config.rfPowerMultiplier
+
+  @Optional.Method( modid = "Mekanism" )
+  def receiveEnergy(container : ItemStack, maxReceive : Int, simulate : Boolean ) : Int = {
+
+    val maxReceivable = (maxReceive / rfPowerMultiplier).toInt
+    val capacity = getChargeDepleted(container)
+    val energyExtracted = Math.min( capacity, Math.min( maxReceivable, maxPowerTransferPerTick ))
+
+    if ( !simulate ) {
+      addCharge(energyExtracted, container)
+    }
+
+    if ( maxReceive == 1 && simulate ) 1 else (energyExtracted * rfPowerMultiplier).toInt
+  }
+
+  @Optional.Method( modid = "Mekanism" )
+  def extractEnergy(container : ItemStack, maxExtract : Int, simulate : Boolean ) : Int = {
+    val maxExtractable = (maxExtract / rfPowerMultiplier).toInt
+    val stored = getMaxCharge(container) - getChargeDepleted(container)
+    val energyExtracted = Math.min( stored, Math.min( maxExtract, maxPowerTransferPerTick ))
+
+    if ( !simulate ) {
+      addCharge(-energyExtracted, container)
+    }
+
+    if ( maxExtract == 1 && simulate ) 1 else (energyExtracted * rfPowerMultiplier).toInt
+  }
+
+  @Optional.Method( modid = "Mekanism" )
+  def getEnergyStored(container : ItemStack) : Int = {
+    val charge = getMaxCharge(container) - getChargeDepleted(container)
+    (charge * rfPowerMultiplier).toInt
+  }
+
+  @Optional.Method( modid = "Mekanism" )
+  def getMaxEnergyStored(container : ItemStack) : Int =
+    (RaygunNbtUtils.getMaxCharge(container) * rfPowerMultiplier).toInt
+}
