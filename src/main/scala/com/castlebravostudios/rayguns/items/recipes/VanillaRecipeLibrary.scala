07e4c9f060d732fd9f321a67ef01a8c22a41803b
Brook Heisler <redattack34@gmail.com>
2014-01-31 18:01:14 -0600

    Corrected a few emitter recipes which were at the wrong tier.

--- src/main/scala/com/castlebravostudios/rayguns/items/recipes/VanillaRecipeLibrary.scala
+++ src/main/scala/com/castlebravostudios/rayguns/items/recipes/VanillaRecipeLibrary.scala
@@ -134,20 +134,22 @@ object VanillaRecipeLibrary extends RecipeLibrary {
       registerChamber( chamber, emitter, DiamondGainMedium, Item.diamond )
 
     registerT1Chamber( Tier1CuttingChamber, Emitters.tier1CuttingEmitter)
+    registerT1Chamber( HeatRayChamber, Emitters.heatRayEmitter)
+    registerT1Chamber( LaserChamber, Emitters.laserEmitter)
+    registerT1Chamber( LightningChamber, Emitters.lightningEmitter)
+
     registerT2Chamber( Tier2CuttingChamber, Emitters.tier2CuttingEmitter)
-    registerT3Chamber( Tier3CuttingChamber, Emitters.tier3CuttingEmitter)
-    registerT3Chamber( DeathRayChamber, Emitters.deathRayEmitter)
-    registerT2Chamber( EnderChamber, Emitters.enderEmitter )
-    registerT3Chamber( ExplosiveChamber, Emitters.explosiveEmitter)
     registerT2Chamber( FortifiedSunlightChamber, Emitters.fortifiedSunlightEmitter)
     registerT2Chamber( FrostRayChamber, Emitters.frostRayEmitter)
-    registerT1Chamber( HeatRayChamber, Emitters.heatRayEmitter)
     registerT2Chamber( ImpulseChamber, Emitters.impulseEmitter)
-    registerT1Chamber( LaserChamber, Emitters.laserEmitter)
     registerT2Chamber( LifeForceChamber, Emitters.lifeForceEmitter)
-    registerT1Chamber( LightningChamber, Emitters.lightningEmitter)
     registerT2Chamber( TractorChamber, Emitters.tractorEmitter)
     registerT2Chamber( MatterTransporterChamber, Emitters.matterTransporterEmitter )
+    registerT2Chamber( EnderChamber, Emitters.enderEmitter )
+
+    registerT3Chamber( Tier3CuttingChamber, Emitters.tier3CuttingEmitter)
+    registerT3Chamber( DeathRayChamber, Emitters.deathRayEmitter)
+    registerT3Chamber( ExplosiveChamber, Emitters.explosiveEmitter)
   }
 
   private def registerEmitters() = {
@@ -172,22 +174,23 @@ object VanillaRecipeLibrary extends RecipeLibrary {
 
     registerT1Emitter( Emitters.laserEmitter, Item.redstone, Item.redstone, Item.redstone, Item.redstone )
     registerT1Emitter( Emitters.heatRayEmitter, Item.coal, Item.bucketLava, Item.coal, Item.bucketLava )
+    registerT1Emitter( Emitters.lightningEmitter, Block.blockIron, Block.blockRedstone, Block.blockIron, Block.blockRedstone )
+    registerT1Emitter( Emitters.shrinkRayEmitter, Block.pistonBase, Block.pistonBase, Block.pistonBase, Block.pistonBase )
+    registerT1Emitter( Emitters.tier1CuttingEmitter, Item.pickaxeStone, Item.shovelStone, Item.pickaxeStone, Item.shovelStone )
+
     registerT2Emitter( Emitters.frostRayEmitter, Block.ice, Block.blockSnow, Block.ice, Block.blockSnow )
     registerT2Emitter( Emitters.lifeForceEmitter, Item.speckledMelon, Item.ghastTear, Item.speckledMelon, Item.ghastTear )
     registerT2Emitter( Emitters.fortifiedSunlightEmitter, Block.wood, Block.wood, Block.wood, Block.wood )
-    registerT3Emitter( Emitters.explosiveEmitter, Block.tnt, Block.tnt, Block.tnt, Block.tnt )
+    registerT2Emitter( Emitters.enderEmitter, Item.enderPearl, Item.enderPearl, Item.enderPearl, Item.enderPearl )
+    registerT2Emitter( Emitters.impulseEmitter, Block.pistonBase, Block.pistonBase, Block.pistonBase, Block.pistonBase )
+    registerT2Emitter( Emitters.tractorEmitter, Block.pistonStickyBase, Block.pistonStickyBase, Block.pistonStickyBase, Block.pistonStickyBase )
+    registerT2Emitter( Emitters.matterTransporterEmitter, Item.enderPearl, Block.pistonBase, Item.enderPearl, Block.pistonBase )
+    registerT2Emitter( Emitters.tier2CuttingEmitter, Item.pickaxeIron, Item.shovelIron, Item.pickaxeIron, Item.shovelIron )
 
     val witherSkull = new ItemStack( Item.skull, 1, 1 )
     registerT3Emitter( Emitters.deathRayEmitter, witherSkull, witherSkull, witherSkull, witherSkull )
-    registerT2Emitter( Emitters.enderEmitter, Item.enderPearl, Item.enderPearl, Item.enderPearl, Item.enderPearl )
-    registerT2Emitter( Emitters.lightningEmitter, Block.blockIron, Block.blockRedstone, Block.blockIron, Block.blockRedstone )
-    registerT2Emitter( Emitters.impulseEmitter, Block.pistonBase, Block.pistonBase, Block.pistonBase, Block.pistonBase )
-    registerT2Emitter( Emitters.tractorEmitter, Block.pistonStickyBase, Block.pistonStickyBase, Block.pistonStickyBase, Block.pistonStickyBase )
-    registerT1Emitter( Emitters.tier1CuttingEmitter, Item.pickaxeStone, Item.shovelStone, Item.pickaxeStone, Item.shovelStone )
-    registerT1Emitter( Emitters.tier2CuttingEmitter, Item.pickaxeIron, Item.shovelIron, Item.pickaxeIron, Item.shovelIron )
+    registerT3Emitter( Emitters.explosiveEmitter, Block.tnt, Block.tnt, Block.tnt, Block.tnt )
     registerT3Emitter( Emitters.tier3CuttingEmitter, Item.pickaxeDiamond, Item.shovelDiamond, Item.pickaxeDiamond, Item.shovelDiamond )
-    registerT1Emitter( Emitters.shrinkRayEmitter, Block.pistonBase, Block.pistonBase, Block.pistonBase, Block.pistonBase )
-    registerT2Emitter( Emitters.matterTransporterEmitter, Item.enderPearl, Block.pistonBase, Item.enderPearl, Block.pistonBase )
   }
 
   private def registerLenses() = {
