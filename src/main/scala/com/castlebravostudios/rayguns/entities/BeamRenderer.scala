5d104cba0dd103f5dc5b8e38be5bea2d4a87d0c9
Brook Heisler <redattack34@gmail.com>
2014-07-14 21:41:16 -0600

    #29 : Fixed rendering bug with non-death, non-lightning beams.

--- src/main/scala/com/castlebravostudios/rayguns/entities/BeamRenderer.scala
+++ src/main/scala/com/castlebravostudios/rayguns/entities/BeamRenderer.scala
@@ -35,6 +35,7 @@ import net.minecraft.client.renderer.entity.Render
 import net.minecraft.entity.Entity
 import net.minecraft.util.ResourceLocation
 import com.castlebravostudios.rayguns.entities.effects.DeathRayEffect
+import org.lwjgl.opengl.GL14
 
 class BeamRenderer extends Render {
 
@@ -52,7 +53,6 @@ class BeamRenderer extends Render {
     GL11.glRotatef(180 - e.rotationPitch, 1.0f, 0.0f, 0.0f)
     GL11.glScalef(0.025f * e.charge.toFloat, 0.025f * e.charge.toFloat, 1.0f)
     GL11.glDisable(GL11.GL_LIGHTING)
-    GL11.glDisable(GL11.GL_CULL_FACE)
     GL11.glEnable(GL11.GL_BLEND);
     if ( e.effect == DeathRayEffect ) {
       GL11.glBlendFunc(GL11.GL_ZERO, GL11.GL_ONE_MINUS_SRC_ALPHA);
@@ -67,27 +67,29 @@ class BeamRenderer extends Render {
 
     val tes = Tessellator.instance
 
-    GL11.glColor4f( 1.0f, 1.0f, 1.0f, 1.0f )
-
     for { _ <- 0 until 3 } {
       tes.startDrawingQuads();
       tes.addVertexWithUV(-1.0D, 0.0D, 0.0D, 0, 0);
       tes.addVertexWithUV(-1.0D, 0.0D, e.length, 0, 1);
       tes.addVertexWithUV(1.0D, 0.0D, e.length, 1, 1);
       tes.addVertexWithUV(1.0D, 0.0D, 0.0D, 1, 0);
+
+      tes.addVertexWithUV(1.0D, 0.0D, 0.0D, 1, 0);
+      tes.addVertexWithUV(1.0D, 0.0D, e.length, 1, 1);
+      tes.addVertexWithUV(-1.0D, 0.0D, e.length, 0, 1);
+      tes.addVertexWithUV(-1.0D, 0.0D, 0.0D, 0, 0);
+
       tes.draw();
       GL11.glRotatef(66.0f, 0.0f, 0.0f, 1.0f)
     }
 
+    GL11.glPopMatrix()
     OpenGlHelper.setActiveTexture(OpenGlHelper.lightmapTexUnit)
     GL11.glEnable(GL11.GL_TEXTURE_2D)
     OpenGlHelper.setActiveTexture(OpenGlHelper.defaultTexUnit)
-    GL11.glColor4f( 1.0f, 1.0f, 1.0f, 1.0f )
     GL11.glDepthMask(true);
     GL11.glDisable(GL11.GL_BLEND)
-    GL11.glEnable(GL11.GL_CULL_FACE)
     GL11.glEnable(GL11.GL_LIGHTING)
-    GL11.glPopMatrix()
   }
 
   def getEntityTexture( e : Entity ) : ResourceLocation = e match {
